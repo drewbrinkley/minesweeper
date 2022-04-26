4267837197a4687fc8eec4ae1db3efd0a346ae1b
Drew Brinkley <drewbrinkley24@gmail.com>
2022-04-26 14:05:45 -0400

    add width_prct function

--- utilities.py
+++ utilities.py
@@ -5,4 +5,6 @@ import settings
 def height_prct(percentage):
     return (settings.HEIGHT / 100) * percentage
 
-print(height_prct(25))
+# create function to calculate percentage of WIDTH
+def width_prct(percentage):
+    return(settings.WIDTH / 100) * percentage
