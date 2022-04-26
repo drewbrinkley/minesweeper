a17dc0c66b8a548615879ea77f3d2ede250a36fc
Drew Brinkley <drewbrinkley24@gmail.com>
2022-04-26 14:06:34 -0400

    update code to set frame dimensions via utilities.py functions

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # import everything from the tkinter library
 from tkinter import *
-
+# import settings.py
 import settings
+# import utilities.py
+import utilities
 
 # create root variable
 root = Tk()
 # change background color of game window
 root.configure(bg="black")
-# set size of game window, length x width
+# set size of game window, length x width are imported from settings
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 # add title to game window
 root.title("Minesweeper Game")
@@ -18,8 +20,10 @@ root.resizable(False, False)
 top_frame = Frame(
     root,
     bg='red', # change later to black after debugging
-    width=1440,
-    height=180
+    # set WIDTH to 100%
+    width=settings.WIDTH,
+    # set height to 25% by calling height_prct() from utilities
+    height=utilities.height_prct(25)
 )
 # place top_frame in window
 top_frame.place(x=0, y=0)
@@ -28,11 +32,13 @@ top_frame.place(x=0, y=0)
 left_frame = Frame(
     root,
     bg='blue', # change to black after debugging
-    width=360,
-    height=540
+    # set width to 25% by calling width_prct()
+    width=utilities.width_prct(25),
+    # set height to 75% by calling height_prct()
+    height=utilities.height_prct(75)
 )
 #place left_frame in window
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utilities.height_prct(25))
 
 # Run the game window
 root.mainloop()
